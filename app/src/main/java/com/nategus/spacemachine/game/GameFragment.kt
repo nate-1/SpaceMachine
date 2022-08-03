40634abe20eccfd5add4ce505e38113e63ff6608
Nathan Wurtz <nathan.wurtz@incotec.fr>
2022-08-03 11:46:09 +0200

    Add background image, improve game fragment style

--- app/src/main/java/com/nategus/spacemachine/game/GameFragment.kt
+++ app/src/main/java/com/nategus/spacemachine/game/GameFragment.kt
@@ -1,19 +1,18 @@
 package com.nategus.spacemachine.game
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.activityViewModels
 import com.google.android.flexbox.*
 import com.nategus.spacemachine.Element
 import com.nategus.spacemachine.databinding.FragmentGameBinding
 
 val titles: List<String> = listOf("cymitharra", "tonsus", "delphinus", "tursio", "Athenae", "Patrae", "aperio", "declinatio", "canis", "flos", "Roma", "Lutetia", "Londinium", "noceo", "nocere", "quisquiliae", "Neapolis", "Antium", "Corinthus", "bellum", "eicio", "gnome", "haruspex", "icon", "moveo", "aniba", "oleum", "speculum", "utinam", "xiphias", "zelus", "porcus_marinus", "Graecia", "operio", "autem", "bos", "helium", "aut", "androgynus", "bovinus", "draco", "equus", "bellicus", "zodiacus", "eques", "formica", "gallina", "porcus", "guerra", "lycophthalmus", "annihilo", "mare", "adalligo", "Aegyptus", "Italia", "Graecus", "tonsor", "oeconomia", "caulis", "homonymus", "illino", "decoctum", "folium", "amicus", "O", "decoquo", "illitus", "sucus", "efficax", "caninus", "sativus", "epiphora", "cauliculus", "tormina", "ulcus", "fimum", "sarcasmus", "caprinus", "vomitio", "inclino", "duo", "tres", "quattuor", "quinque", "primus", "tripus", "septem", "ligatura", "anthropophagus", "octo", "octavus", "unio", "novem", "decem", "chiliometrum", "epigramma", "centum", "mille", "quintus", "nox", "quartus", "tertius", "macropus", "septimus", "homo", "nonus", "hippotoxotae", "synonymum", "Iaponia", "phthir", "diphthongus", "ab", "abdo", "abeo", "abhinc", "abicio", "abigo", "aboleo", "abscedo", "sol", "absens", "absisto", "absolvo", "abstinens", "abstineo", "abstuli", "prior", "novies", "smaragdus", "absum", "absurdus", "prae", "abundo", "Tocio", "frater", "eo", "accedo", "res", "secundus", "accendo", "sextus", "quincuplex", "accido", "simplex", "Hoccaido", "Ocinava", "beryllium", "accipio", "Zoroastreus", "Zopyriatim", "lithium", "accommodo", "caerimonia", "dies_Solis", "dies_Lunae", "dies_Martis", "uranium", "Mars", "nex", "accumbo", "Kalendae", "hydrogenium", "Iuppiter", "nux", "accuso", "palladium", "pirata", "acer", "Venus", "terbium", "Saturnus", "homunculus", "sum", "actinium", "Pluto", "daemon", "acerbus", "borum", "volcanus", "diabolus", "magnesium", "Mercurius", "gladius", "acervus", "Achaicus", "ununseptium", "Iuno", "elementum", "cupido", "ignis", "vanitas", "manatus", "neon", "achates", "Demeter", "lacrimo", "Acheron", "argon", "luna", "Minerva", "Achilles", "ununoctium", "lupus", "radon", "ununbium", "ununhexium", "ununpentium", "ununquadium", "acies", "nix", "cornix", "Kyotum", "leo", "Argentina", "Nara", "Ciusium", "unununium", "ununtrium", "tellurium", "barium", "platinum", "berkelium", "Nagasacium", "Fusius", "nomas", "Vindobona", "nobelium", "nomen", "Meacum", "Osaca", "Hirosima", "acriter", "cadmium", "orca", "Varsovia", "Hieus", "Lotharingia", "neptunium")
 val instructionSentences: List<String> = listOf("I think you should", "You gotta", "You better", "You probably should", "You need to")
-const val LIST_ITEM_SIZE: Int = 4
 class GameFragment : Fragment() {
 
     private lateinit var binding: FragmentGameBinding
@@ -22,19 +21,18 @@ class GameFragment : Fragment() {
     private lateinit var supportFragmentManager: FragmentManager
     private var _supportFragmentManager: FragmentManager? = null
 
-    //private val gameViewModel: GameViewModel by activityViewModels()
-
     private var usedTitles: List<String> = emptyList()
 
     private var buttons: MutableList<Element> = emptyList<Element>().toMutableList()
     private var switches: MutableList<Element> = emptyList<Element>().toMutableList()
 
-    private var currentElementId: Int = 0;
+    private var currentElementId: Int = 0
     private var life: Int = 100
+    private var listItemSize: Int = 5
+    private var wave: Int = 1
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var timer: CountDownTimer
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,6 +46,7 @@ class GameFragment : Fragment() {
         supportFragmentManager = _supportFragmentManager!!
 
         setUpGameBoard()
+        
         return binding.root
     }
 
@@ -59,9 +58,9 @@ class GameFragment : Fragment() {
 
     fun setUpGameBoard() {
 
-        for(i in (1..LIST_ITEM_SIZE)) {
+        for(i in (1..listItemSize)) {
             switches += Element(i, getTitle(),false)
-            buttons += Element(i+LIST_ITEM_SIZE, getTitle(), false)
+            buttons += Element(i+listItemSize, getTitle(), false)
         }
 
         binding.switchRecyclerView.adapter = SwitchAdaptor(switches) { position, isChecked ->
@@ -117,6 +116,8 @@ class GameFragment : Fragment() {
     fun checkAction(el: Element) {
         if(el.id == currentElementId) {
             generateAction()
+            wave++
+
         }
         else {
             life -= 5
